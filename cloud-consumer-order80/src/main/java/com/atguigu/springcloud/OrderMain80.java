4d81b8bab875c0196af164134f945923e43f6ca8
mucongcong <mucongcong@corp.netease.com>
2023-03-01 12:58:53 +0800

    [spring-cloud]order80初始化

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mucongcong
+ * @date 2023/03/01 12:52
+ * @since
+ **/
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+        System.out.println("启动成功");
+    }
+}
---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mucongcong
+ * @date 2023/03/01 12:55
+ * @since
+ **/
+@RestController
+@Slf4j
+public class OrderController {
+}
