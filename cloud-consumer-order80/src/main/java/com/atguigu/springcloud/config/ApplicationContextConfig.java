bd2da1e58dd5a35490f48d52c4b585af61e48d0c
mucongcong <mucongcong@corp.netease.com>
2023-03-01 23:49:39 +0800

    [spring-cloud]order80完成

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author mucongcong
+ * @date 2023/03/01 22:39
+ * @since
+ **/
+@Configuration
+public class ApplicationContextConfig {
+    /**
+     * restful 风格接口调用
+     **/
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
